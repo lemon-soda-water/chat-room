917a136cace9c20160c1907189023d4f3dd8e48b
期待我的征服 <3424371897@qq.com>
2022-09-22 17:30:57 +0800

    修改请求地址

--- public/src/utils/http.js
+++ public/src/utils/http.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 axios.defaults.baseURL = 'http://localhost:5000'
 
 export const createUser = (values) => {
-  return axios.post('/api/auth/login', values)
+  return axios.post('/api/auth/register', values)
 }
