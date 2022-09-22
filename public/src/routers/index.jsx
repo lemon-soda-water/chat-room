01a8d3cf37c13cb3f8fcb142fdd6505d3b6b0993
期待我的征服 <3424371897@qq.com>
2022-09-22 14:40:34 +0800

    add base pages and base route

--- public/src/routers/index.jsx
+++ public/src/routers/index.jsx
@@ -1,9 +1,19 @@
-import Register from "../components/Register";
+import Register from "../pages/Register";
+import Chat from "../pages/Chat";
+import Login from "../pages/Login";
 
 const routes = [
   {
     path: "/register",
     element: <Register />,
+  },
+  {
+    path: "/",
+    element: <Chat />,
+  },
+  {
+    path: "/Login",
+    element: <Login />,
   },
 ];
 
