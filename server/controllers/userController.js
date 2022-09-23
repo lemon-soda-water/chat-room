bc4d87f7f5399c31e42fcd040a5ced46d8161900
期待我的征服 <3424371897@qq.com>
2022-09-23 22:24:35 +0800

    实现请求连续人路由功能

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -3,29 +3,25 @@ const brcypt = require("bcrypt");
 
 module.exports.register = async (req, res, next) => {
   try {
-    const {
-      username,
-      password,
-      email
-    } = req.body;
+    const { username, password, email } = req.body;
 
     const usernameCheck = await User.findOne({
-      username
+      username,
     });
     if (usernameCheck) {
       return res.json({
         msg: "用户名已经存在",
-        status: false
+        status: false,
       });
     }
 
     const emailCheck = await User.findOne({
-      email
+      email,
     });
     if (emailCheck) {
       return res.json({
         msg: "邮箱已经存在",
-        status: false
+        status: false,
       });
     }
 
@@ -39,7 +35,7 @@ module.exports.register = async (req, res, next) => {
     delete user.password;
     return res.json({
       status: true,
-      user
+      user,
     });
   } catch (error) {
     next(error);
@@ -48,34 +44,31 @@ module.exports.register = async (req, res, next) => {
 
 module.exports.login = async (req, res, next) => {
   try {
-    const {
-      username,
-      password
-    } = req.body;
+    const { username, password } = req.body;
 
     const user = await User.findOne({
-      username
+      username,
     });
 
     if (!user) {
       return res.json({
         msg: "用户名或密码错误",
-        status: false
+        status: false,
       });
     }
 
-    const isPasswordValid = await brcypt.compare(password, user.password)
+    const isPasswordValid = await brcypt.compare(password, user.password);
     if (!isPasswordValid) {
       return res.json({
         msg: "用户名或密码错误",
-        status: false
+        status: false,
       });
     }
 
     delete user.password;
     return res.json({
       status: true,
-      user
+      user,
     });
   } catch (error) {
     next(error);
@@ -88,12 +81,26 @@ module.exports.setAvatar = async (req, res, next) => {
     const avatarImage = req.body.image;
     const userData = await User.findByIdAndUpdate(id, {
       isAvatarImageSet: true,
-      avatarImage
-    })
+      avatarImage,
+    });
     return res.json({
       isSet: userData.isAvatarImageSet,
-      image: userData.avatarImage
-    })
+      image: userData.avatarImage,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.getAllContacts = async (req, res, next) => {
+  try {
+    const users = await User.find({ _id: { $ne: req.params.id } }).select([
+      "email",
+      "username",
+      "avatarImage",
+      "_id",
+    ]);
+    return res.json(users);
   } catch (error) {
     next(error);
   }
