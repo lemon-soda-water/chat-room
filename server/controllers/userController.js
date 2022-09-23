675002622158b7ea48dc5b0349f3e798591cd6d3
期待我的征服 <3424371897@qq.com>
2022-09-23 16:00:12 +0800

    用户头像设置功能

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -3,16 +3,30 @@ const brcypt = require("bcrypt");
 
 module.exports.register = async (req, res, next) => {
   try {
-    const { username, password, email } = req.body;
+    const {
+      username,
+      password,
+      email
+    } = req.body;
 
-    const usernameCheck = await User.findOne({ username });
+    const usernameCheck = await User.findOne({
+      username
+    });
     if (usernameCheck) {
-      return res.json({ msg: "用户名已经存在", status: false });
+      return res.json({
+        msg: "用户名已经存在",
+        status: false
+      });
     }
 
-    const emailCheck = await User.findOne({ email });
+    const emailCheck = await User.findOne({
+      email
+    });
     if (emailCheck) {
-      return res.json({ msg: "邮箱已经存在", status: false });
+      return res.json({
+        msg: "邮箱已经存在",
+        status: false
+      });
     }
 
     const hashedPassword = await brcypt.hash(password, 10);
@@ -23,7 +37,10 @@ module.exports.register = async (req, res, next) => {
     });
     // 创建用户，数据库会返回所有信息
     delete user.password;
-    return res.json({ status: true, user });
+    return res.json({
+      status: true,
+      user
+    });
   } catch (error) {
     next(error);
   }
@@ -31,21 +48,52 @@ module.exports.register = async (req, res, next) => {
 
 module.exports.login = async (req, res, next) => {
   try {
-    const { username, password } = req.body;
+    const {
+      username,
+      password
+    } = req.body;
 
-    const user = await User.findOne({ username });
+    const user = await User.findOne({
+      username
+    });
 
     if (!user) {
-      return res.json({ msg: "用户名或密码错误", status: false });
+      return res.json({
+        msg: "用户名或密码错误",
+        status: false
+      });
     }
 
     const isPasswordValid = await brcypt.compare(password, user.password)
     if (!isPasswordValid) {
-      return res.json({ msg: "用户名或密码错误", status: false });
+      return res.json({
+        msg: "用户名或密码错误",
+        status: false
+      });
     }
 
     delete user.password;
-    return res.json({ status: true, user });
+    return res.json({
+      status: true,
+      user
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.setAvatar = async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const avatarImage = req.body.image;
+    const userData = await User.findByIdAndUpdate(id, {
+      isAvatarImageSet: true,
+      avatarImage
+    })
+    return res.json({
+      isSet: userData.isAvatarImageSet,
+      image: userData.avatarImage
+    })
   } catch (error) {
     next(error);
   }
